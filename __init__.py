c37119ab99cf31c359fdc70d77d915798386a904
VICTORVICKIE <s.vickie14@gmail.com>
2021-05-11 07:33:43 +0530

    Added Page Curl

--- __init__.py
+++ __init__.py
@@ -28,7 +28,7 @@ __all__ = (
 )
 
 from kivy.uix.screenmanager import ShaderTransition
-from kivy.properties import StringProperty, OptionProperty, NumericProperty
+from kivy.properties import StringProperty, OptionProperty, NumericProperty, ColorProperty
 from kivy.resources import resource_find, resource_add_path
 from os.path import dirname
 
@@ -521,6 +521,98 @@ class ShatterTransition(ShaderTransition):
             self.fs = self.SHATTER_TRANSITION_UP
         elif largs[1] == 'down':
             self.fs = self.SHATTER_TRANSITION_UP
+
+
+class PageCurlTransition(ShaderTransition):
+    direction = OptionProperty("Bottom_to_Top", options=["Bottom_to_Top", "Top_to_Bottom"])
+
+    fs = """
+    $HEADER$
+
+    #define pi 3.14159265359
+    #define radius .1
+
+    uniform float t;
+    uniform float direction;
+    uniform float aspect;
+    uniform vec2 resolution;
+    uniform sampler2D tex_in;
+    uniform sampler2D tex_out;
+
+    //IDK why but need to remap it to work, if something doesnt works try remap xD
+    float map(float value)
+    {
+      float low_map_from = 0., high_map_from = 1., low_map_to = 0.075, high_map_to = -1.15;
+      return low_map_to + (value - low_map_from) * (high_map_to - low_map_to) / (high_map_from - low_map_from);
+    }
+
+    void main( void )
+    {
+        float aspect_ratio = 0.0;
+        if (aspect == 1.0) {aspect_ratio = resolution.x / resolution.y;}
+        else {aspect_ratio = resolution.y / resolution.x; }
+
+        vec2 uv = gl_FragCoord.xy/resolution.xy;
+        vec2 dir = vec2(0.15,-1.0);
+        vec2 origin = vec2(0.0,0.0);
+        
+        float move = 0.;
+        if (direction == 1.0) {move = map(t);}
+        else {move = map(1.0 - t);}
+        
+
+        float proj = dot(uv - origin, dir);
+        float dist = proj - move ;
+        
+        vec2 linePoint = uv - dist * dir ;
+        
+        if (dist > radius)
+        {
+            if (direction == 1.0) {gl_FragColor = texture2D(tex_in, uv);}
+            else{gl_FragColor = texture2D(tex_out, uv);}
+
+            gl_FragColor.rgb *= pow(clamp(dist - radius, 0., 1.) * 1.5, .2);
+        }
+        else if (dist >= 0.)
+        {
+            float theta = asin(dist / radius);
+            vec2 p2 = linePoint + dir * (pi - theta) * radius;
+            vec2 p1 = linePoint + dir * theta * radius;
+            uv = (p2.x <= aspect_ratio && p2.y <= 1. && p2.x > 0. && p2.y > 0.) ? p2 : p1;
+
+            if (direction == 1.0) {gl_FragColor = texture2D(tex_out, uv);}
+            else {gl_FragColor = texture2D(tex_in, uv);}
+
+            gl_FragColor.rgb *= pow(clamp((radius - dist) / radius, 0., 1.), .2);
+        }
+        else 
+        {
+            vec2 p = linePoint + dir * (abs(dist) + pi * radius) ;
+            uv = (p.x <= aspect_ratio && p.y <= 1. && p.x > 0. && p.y > 0.) ? p : uv;
+            
+            if (direction == 1.0) {gl_FragColor = texture2D(tex_out, uv);}
+            else {gl_FragColor = texture2D(tex_in, uv);}
+        }
+    }
+    """
+
+    fs = StringProperty(fs)
+    clearcolor = ColorProperty([0, 0, 0, 0])
+
+    def add_screen(self, screen):
+        super().add_screen(screen)
+
+        self.render_ctx["resolution"] = list(map(float, screen.size))
+
+        aspect_ratio = screen.size[0]/screen.size[1]
+        
+        self.render_ctx["aspect"] = 1.0 * (aspect_ratio > 1.0) + 2.0 * (1.0 >= aspect_ratio)
+
+        if self.direction == "Bottom_to_Top":
+            self.render_ctx["direction"] = 1.0
+        
+        else:
+            self.render_ctx["direction"] = 2.0
 
 
 
